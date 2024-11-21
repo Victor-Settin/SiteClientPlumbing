c70fd227d7f727fbeb80f81e0346aed5e9af4f90
Victor Settin <victor.profissional22@gmail.com>
2024-11-20 18:33:01 -0800

    alterado pagina principal

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
-import Gallery from './pages/Gallery';  // Importando a nova página de galeria
-import Footer from './components/Footer';
-import Header from './components/Header';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Services from './pages/Services';
+// import Contact from './pages/Contact';
+// import Gallery from './pages/Gallery';  // Importando a nova página de galeria
+// import Footer from './components/Footer';
+// import Header from './components/Header';
+import Coming from './pages/Coming';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
+    // <Router>
+    //   <div className="App">
+    //     <Header />
         
-        {/* Definindo as rotas principais */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/gallery/:serviceName" element={<Gallery />} />  {/* Rota para a galeria */}
-        </Routes>
+    //     {/* Definindo as rotas principais */}
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/about" element={<About />} />
+    //       <Route path="/services" element={<Services />} />
+    //       <Route path="/contact" element={<Contact />} />
+    //       <Route path="/gallery/:serviceName" element={<Gallery />} />  {/* Rota para a galeria */}
+    //     </Routes>
         
-        {/* Rodapé que será exibido em todas as rotas */}
-        <Footer />
-      </div>
-    </Router>
+    //     {/* Rodapé que será exibido em todas as rotas */}
+    //     <Footer />
+    //   </div>
+    // </Router>
+    <Coming />
   );
 }
 
---
+++ src/pages/Coming.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Container from '../components/Container'; // Importando o Container
+import '../styles/About.css'; // Importando o CSS atualizado
+
+function About() {
+  return (
+    <Container> {/* Envolvendo todo o conteúdo com o Container */}
+      <div >
+        its coming
+      </div>
+    </Container>
+  );
+}
+
+export default About;
