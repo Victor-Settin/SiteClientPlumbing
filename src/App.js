af2494390e1bc506979da3fa991e72e7b6a34881
Victor Settin <victor.profissional22@gmail.com>
2025-01-06 18:33:46 -0800

    att

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
 import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Home from './pages/Home';
-// import About from './pages/About';
-// import Services from './pages/Services';
-// import Contact from './pages/Contact';
-// import Gallery from './pages/Gallery';  // Importando a nova página de galeria
-// import Footer from './components/Footer';
-// import Header from './components/Header';
-import Coming from './pages/Coming';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react'; // Importe o Analytics
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import Gallery from './pages/Gallery';  
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
-    // <Router>
-    //   <div className="App">
-    //     <Header />
+    <Router>
+      <div className="App">
+        <Header />
         
-    //     {/* Definindo as rotas principais */}
-    //     <Routes>
-    //       <Route path="/" element={<Home />} />
-    //       <Route path="/about" element={<About />} />
-    //       <Route path="/services" element={<Services />} />
-    //       <Route path="/contact" element={<Contact />} />
-    //       <Route path="/gallery/:serviceName" element={<Gallery />} />  {/* Rota para a galeria */}
-    //     </Routes>
+        {/* Definindo as rotas principais */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/gallery/:serviceName" element={<Gallery />} />
+        </Routes>
+        <Footer />
         
-    //     {/* Rodapé que será exibido em todas as rotas */}
-    //     <Footer />
-    //   </div>
-    // </Router>
-    <Coming />
+        {/* Adicione o Analytics aqui */}
+        <Analytics />
+      </div>
+    </Router>
   );
 }
 
